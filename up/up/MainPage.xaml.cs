1e9dbe37a2f4fb42506a9b0c483eeacc10c31cfd
jasaga2401 <jasaga@protonmail.com>
2023-12-20 21:27:34 +0000

    Add project files.

---
+++ up/up/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace up
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            //MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void OnSignInButtonClicked(object sender, EventArgs e)
+        {
+            string username = usernameEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (username == "admin" && password == "passwd")
+            {
+                DisplayAlert("Login", "Login success", "OK");                
+                Application.Current.MainPage = new HomePage();
+
+                //NavigationPage HomePage = new NavigationPage(new HomePage());
+                //await Navigation.PushAsync(new HomePage());
+            }
+            else
+            {                
+                DisplayAlert("Login", "Login failed", "OK");                
+                passwordEntry.Text = string.Empty;
+            }
+        }
+    }
+}
